6f747a61fa591ea94b82eb61cc25d9efa17b46ee
RyanOfEastview <ryanmccarthy613@gmail.com>
2022-08-18 14:48:38 -0400

    connected front end to back end

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 const fs = require('fs');
 const path = require('path');
-
 const express = require('express');
 const { animals } = require('./data/animals');
-const { setFlagsFromString } = require('v8');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-//parse incoming string or array data
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-
-//parse incoming JSON data
 app.use(express.json());
 
-function filterByQuery(query, animalsArray) {
+function filterByQuery(query, animalsArray) { 
   let personalityTraitsArray = [];
   let filteredResults = animalsArray;
   if (query.personalityTraits) {
@@ -50,11 +46,10 @@ function createNewAnimal(body, animalsArray) {
   const animal = body;
   animalsArray.push(animal);
   fs.writeFileSync(
-  path.join(__dirname, './data/animals.json'),
-  JSON.stringify({ animals: animalsArray }, null, 2)
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
   );
   return animal;
-
 }
 
 function validateAnimal(animal) {
@@ -94,13 +89,28 @@ app.post('/api/animals', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
 
-  // if any data in req.body is incorrect, send 400 error back
   if (!validateAnimal(req.body)) {
     res.status(400).send('The animal is not properly formatted.');
   } else {
     const animal = createNewAnimal(req.body, animals);
     res.json(animal);
   }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
